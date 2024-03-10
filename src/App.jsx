20678cd6c99f8dbd18275da7ff041775c3f87577
Phil Curran <pecurran@outlook.com>
2024-03-10 10:03:50 -0700

    final result.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
-import { Router, Routes } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import getAllStarships from "./services/swapi";
 
 export default function App() {
+  const [starships, setStarships] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getAllStarships();
+        setStarships(data.results);
+      } catch (error) {
+        console.error("Error fetching starships:", error);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="app">
-      <p>hello world.</p>
+      <Header />
+      <div className="container">
+        {starships.map((starship) => (
+          <Card key={starship.uid} starship={starship} />
+        ))}
+      </div>
     </div>
   );
 }
